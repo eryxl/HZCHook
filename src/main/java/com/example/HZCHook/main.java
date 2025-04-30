0c93b26910765d6f1dd646d2e1fef80bc90694a0
Eryx-N <130699959+eryxl@users.noreply.github.com>
2025-04-30 12:01:39 -0400

    Create main.java

---
+++ src/main/java/com/example/HZCHook/main.java
@@ -0,0 +1,24 @@
+package com.example.hzchook;
+
+import ch.njol.skript.SkriptPlugin;
+import ch.njol.skript.classes.Changer.ChangeMode;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+
+/**
+ * HZCHook - A Skript plugin for sending webhooks to external servers
+ * 
+ * Features:
+ * - Send JSON data to any webhook endpoint
+ * - Supports complex data structures including maps and lists
+ * - Built-in error handling and logging
+ * - Compatible with Skript 2.7.3
+ */
+public class HZCHook extends SkriptPlugin {
+    @Override
+    public void onLoad() {
+        // Register our webhook syntax
+        WebhookSyntax.register(this);
+    }
+}
